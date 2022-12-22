ecfc03e7d8b5172753a6455ea34db3709e2956d2
Chiemezie Udoh <chimexes@gmail.com>
2022-12-22 11:26:05 +0100

    Add files via upload

---
+++ ModuleInstall.bat
@@ -0,0 +1,21 @@
+SET PipPath="C:\Users\q38226\AppData\Local\Programs\Python\Python38\Scripts\pip.exe"
+SET PythonPath="C:\Users\q38226\AppData\Local\Programs\Python\Python38\python.exe"
+%PythonPath% %PipPath% install matplotlib
+%PythonPath% %PipPath% install pyautogui
+%PythonPath% %PipPath% install xlrd
+%PythonPath% %PipPath% install Beautifulsoup
+%PythonPath% %PipPath% install selenium
+%PythonPath% %PipPath% install flask
+%PythonPath% %PipPath% install django
+%PythonPath% %PipPath% install openpyxl
+%PythonPath% %PipPath% install PIL
+%PythonPath% %PipPath% install gitpython
+%PythonPath% %PipPath% install numpy
+%PythonPath% %PipPath% install pandas
+%PythonPath% %PipPath% install opencv-python
+%PythonPath% %PipPath% install pdf
+%PythonPath% %PipPath% install xlwt
+%PythonPath% %PipPath% install runpy
+
+
+
